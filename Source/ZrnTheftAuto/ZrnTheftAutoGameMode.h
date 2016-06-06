d84bbb12fbe2f9f5d39d9118297700871d254071
Zero-One101 <stephenbadrick@gmail.com>
2016-06-06 18:37:50 +0100

    Initial commit. Added Advanced Vehicle template

---
+++ Source/ZrnTheftAuto/ZrnTheftAutoGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "ZrnTheftAutoGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AZrnTheftAutoGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	AZrnTheftAutoGameMode();
+};
+
+
+
